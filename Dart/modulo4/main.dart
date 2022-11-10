09c5898272ea0b2160c365b3d5a5f9c223badf84
victorvancan <victorvancan1@gmail.com>
2022-11-10 16:38:45 -0300

    estudos 10/11/22

---
+++ Dart/modulo4/main.dart
@@ -0,0 +1,44 @@
+import 'controllers/bank_controller.dart';
+import 'exceptitions/bank_controller_exceptions.dart';
+import 'models/account.dart';
+
+void main() {
+  //assert(3 > 4);
+  //assert(3 > 4, "Que pena, 3 não é maior que 4");
+
+  // Criando o banco
+  BankController bankController = BankController();
+
+  Account testAccount = Account(name: "", balance: 0, isAuthenticated: true);
+
+  // Adicionando contas
+  bankController.addAccount(
+      id: "Ricarth",
+      account:
+      Account(name: "Ricarth Lima", balance: 400, isAuthenticated: false));
+
+  bankController.addAccount(
+      id: "Kako",
+      account:
+      Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+  // Fazendo transferência
+  try {
+    bankController.makeTransfer(
+        idSender: "Kako", idReceiver: "Ricarth", amount: 200);
+
+    print("Transação concluída com sucesso");
+  } on SenderIdInvalidException catch (e) {
+    print(e.message);
+  } on ReceiverIdInvalidException catch (e) {
+    print(e);
+  } on SenderNotAuthenticatedException catch (e) {
+    print(e);
+  } on ReceiverNotAuthenticatedException catch (e) {
+    print(e);
+  } on SenderBalanceLowerThanAmountException catch (e) {
+    print(e);
+  } catch (e) {
+    print("Erro desconhecido.");
+  }
+}
