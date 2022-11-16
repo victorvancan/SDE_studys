e7b6e8be8183fe90ec5ce6d8a209fb73e27b8f9a
victorvancan <victorvancan1@gmail.com>
2022-11-16 17:57:04 -0300

    estudos 16/11/22

--- estudosflutter/lib/main.dart
+++ estudosflutter/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   // This widget is the root of your application.
   @override
@@ -20,30 +27,42 @@ class MyApp extends StatelessWidget {
           title: const Text('Tarefas'),
           leading: Container(),
         ),
-        body: ListView(
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Tasks(
-                'Estudar Flutter',
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(microseconds: 800),
+          child: ListView(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: Tasks(
+                    'Estudar Flutter',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    4),
               ),
-            ),
-            Tasks(
-              'Andar de Bike',
-            ),
-            Tasks(
-              'Ler',
-            ),
-            Tasks(
-              'Meditar',
-            ),
-            Tasks(
-              'Jogar',
-            )
-          ],
+              Tasks(
+                  'Andar de Bike',
+                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                  2),
+              Tasks(
+                  'Ler',
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  3),
+              Tasks(
+                  'Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  1),
+              Tasks('Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 0)
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;  
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
@@ -52,14 +71,16 @@ class MyApp extends StatelessWidget {
 
 class Tasks extends StatefulWidget {
   final String nome;
-  const Tasks(this.nome,{Key? key}) : super(key: key);
+  final String foto;
+  final int dificuldade;
+  const Tasks(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
 
   @override
   State<Tasks> createState() => _TasksState();
 }
 
 class _TasksState extends State<Tasks> {
-
   @override
   int nivel = 0;
   Widget build(BuildContext context) {
@@ -88,15 +109,59 @@ class _TasksState extends State<Tasks> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Container(width: 72,height: 100,color: Colors.black26,),
                       Container(
-                        width: 200,
-                        child: Text(
-                          widget.nome,
-                          style: const TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis),
-                        ),
+                          width: 72,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              color: Colors.black26,
+                              borderRadius: BorderRadius.circular(4)),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image.network(widget.foto,
+                                  fit: BoxFit.cover))),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: const TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: widget.dificuldade >= 1
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: widget.dificuldade >= 2
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: widget.dificuldade >= 3
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: widget.dificuldade >= 4
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: widget.dificuldade >= 5
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                            ],
+                          ),
+                        ],
                       ),
                       Padding(
                         padding: const EdgeInsets.all(16.0),
@@ -104,11 +169,10 @@ class _TasksState extends State<Tasks> {
                           height: 52,
                           width: 52,
                           child: ElevatedButton(
-                            onPressed: (){
-                              setState((){
+                            onPressed: () {
+                              setState(() {
                                 nivel++;
                               });
-
                             },
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -138,7 +202,9 @@ class _TasksState extends State<Tasks> {
                         width: 200,
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel/10,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
                         ),
                       ),
                     ),
@@ -159,4 +225,3 @@ class _TasksState extends State<Tasks> {
     );
   }
 }
-
