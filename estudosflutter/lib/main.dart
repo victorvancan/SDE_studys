1234d6d2e41b1e708aa37d84d7babdf5c860e078
victorvancan <victorvancan1@gmail.com>
2022-11-18 18:30:47 -0300

    estudos 18/11/22

--- estudosflutter/lib/main.dart
+++ estudosflutter/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:estudosflutter/screens/form_screem.dart';
 import 'package:estudosflutter/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:const InitialScreen(),
+      home:const FormScreen(),
     );
   }
 }
