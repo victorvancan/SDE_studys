827664091bf00278cf295ca42de04160536f92fb
victor vançan <74362179+victorvancan@users.noreply.github.com>
2023-02-20 11:39:17 -0300

    estudos hibernate 20/02/23

---
+++ Java/Curso_Java/src/br/com/loja-hibernate/src/main/java/br/com/loja/teste/CadastroDeProduto.java
@@ -0,0 +1,27 @@
+package br.com.loja.teste;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.loja.modelo.Produto;
+
+public class CadastroDeProduto {
+	public static void main(String[] args) {
+		Produto celular = new Produto();
+
+		celular.setNome("Xiaomi Redmi");
+		celular.setDescricao("Muito legal");
+		celular.setPreco(new BigDecimal("800"));
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(celular);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
