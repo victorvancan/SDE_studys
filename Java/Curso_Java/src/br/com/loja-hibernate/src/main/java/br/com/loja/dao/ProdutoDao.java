5c93e55068f96c9519ee9bc6f1b92e23300eaf8a
victor vançan <74362179+victorvancan@users.noreply.github.com>
2023-02-24 18:38:42 -0300

    estudos 24/02/23

--- Java/Curso_Java/src/br/com/loja-hibernate/src/main/java/br/com/loja/dao/ProdutoDao.java
+++ Java/Curso_Java/src/br/com/loja-hibernate/src/main/java/br/com/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.loja.modelo.Produto;
@@ -20,4 +22,28 @@ public class ProdutoDao {
 	{
 		this.em.persist(produto);
 	}
+	
+	public Produto buscarPorId(long id) {
+	return em.find(Produto.class, id);
+	}
+	
+	public List<Produto> buscarTodos()
+	{	
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+	
+	public List<Produto> buscarPorNome(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                        .getResultList();
+	}
+    public List<Produto> buscarPorNomeDaCategoria(String nome) {
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
 }
--- Java/Curso_Java/src/br/com/loja-hibernate/src/main/java/br/com/loja/teste/CadastroDeProduto.java
+++ Java/Curso_Java/src/br/com/loja-hibernate/src/main/java/br/com/loja/teste/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.loja.teste;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -12,6 +13,21 @@ import br.com.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
+		cadastrarProduto();
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(produto -> System.out.println(p.getNome()));
+		
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celualres = new Categoria("Celulares");
 		Produto celular = new Produto("Xiaomi Redmi","Muito legal",new BigDecimal("800"), celualres);
 
