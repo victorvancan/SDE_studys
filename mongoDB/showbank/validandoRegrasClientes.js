3de0031c319fdb4f6fe64a2650daad551a595ded
victor vançan <74362179+victorvancan@users.noreply.github.com>
2023-03-24 18:10:30 -0300

    estudos 24/03/23

---
+++ mongoDB/showbank/validandoRegrasClientes.js
@@ -0,0 +1,45 @@
+let clientes = {
+       $jsonSchema: {
+      bsonType: "object",
+       "additionalProperties": false,
+      required: ["_id", "nome", "cpf",  "status_civil", "data_nascimento", "endereco","genero", "profissao" ],
+      properties: {
+            _id:{
+              bsonType: "objectId",
+              description: "informe corretamente o endereço do cliente" 
+          },
+         nome: {
+            bsonType: "string",
+            maxLength: 150,
+            description: "informe corretamente o nome do cliente"
+         },
+         cpf: {
+            bsonType: "string",
+            minLength: 14,
+            maxLength: 150,
+            description: "informe corretamente o cpf do cliente"
+         },
+         status_civil: {
+            enum: [ "Solteiro(a)", "Casado(a)", "Separado(a)", "Divorciado(a)", "Viúvo(a)" ],
+            description: "informe corretamente o status civil do cliente"
+         },
+         data_nascimento: {
+            bsonType: ["string", "null"],
+            description: "informe corretamente a data de nascimento do cliente"
+         },
+         endereco: {
+            bsonType: "string",
+            description: "informe corretamente o endereço do cliente"
+                },
+         genero:{
+              bsonType: "string",
+              description: "informe corretamente o genero do cliente" 
+          },
+         profissao:{
+              bsonType: "string",
+              description: "informe corretamente o profissão do cliente" 
+          }
+            }
+        }
+    
+}
