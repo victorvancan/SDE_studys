3ffe77b7441f890bc4af7cb333a08bb96fc9efd5
victor vancan <victorvancan1@gmail.com>
2023-08-31 08:41:38 -0300

    estudos 31/08/23

--- JS/aluramidi/main.js
+++ JS/aluramidi/main.js
@@ -3,7 +3,6 @@ function tocaSomPom ()
     document.querySelector('#som_tecla_pom').play();
 }
 
+ const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
- document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
+listaDeTeclas[0].onclick = tocaSomPom;
