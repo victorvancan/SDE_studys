949719a9b22b5aefa1cde6293f62b9c1b4b71bc3
victor vancan <victorvancan1@gmail.com>
2023-09-04 19:49:43 -0300

    estudos 04/09/23

--- JS/aluramidi/main.js
+++ JS/aluramidi/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idTagAudio) {
-    document.querySelector(idTagAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        alert('Elemento nao encontrado ou seletor n encontrado')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,6 +14,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
+    const tecla = listaDeTeclas[contador];
     const instrumento = listaDeTeclas[contador].classList[1];
     //template string
     const idAudio = `#som_${instrumento}`;
@@ -15,4 +23,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio)
     }
 
+    tecla.onkeydown = function (evento) {
+        if (evento.code == 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
