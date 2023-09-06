3bd183d79ed3aca419f535a6dd993e3aff3590de
victor vancan <victorvancan1@gmail.com>
2023-09-06 10:08:36 -0300

    estudos 06/09/23

--- JS/Fokus-projeto-base/script.js
+++ JS/Fokus-projeto-base/script.js
@@ -1,6 +1,11 @@
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+
+
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
@@ -9,3 +14,63 @@ focoBt.addEventListener('click', () => {
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
 })
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+})
+
+// focoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'foco')
+//     banner.setAttribute('src', '/imagens/foco.png')
+// })
+
+// curtoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-curto')
+//     banner.setAttribute('src', '/imagens/descanso-curto.png')
+// })
+
+// longoBt.addEventListener('click', () => {
+//     html.setAttribute('data-contexto', 'descanso-longo')
+//     banner.setAttribute('src', '/imagens/descanso-longo.png')
+// })
+focoBt.addEventListener('click', () => {
+    alterarContexto('foco')
+})
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+})
+longoBt.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+})
+
+
+
+function alterarContexto() {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
+
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;
+    }
+}
