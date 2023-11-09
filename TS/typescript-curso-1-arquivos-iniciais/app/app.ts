fe78b58dc4146621336ec870a48e1da6b5a4c065
victor vancan <victorvancan1@gmail.com>
2023-11-08 23:01:48 -0300

    estudos  08/11/23

--- TS/typescript-curso-1-arquivos-iniciais/app/app.ts
+++ TS/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,6 +1,9 @@
-import { Negociacao } from './models/negociacao.js';
+import { negociacaoController } from "./controllers/negociacaoContreoller.js"
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-
-
-
+const controller = new negociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event =>
+{
+    event.preventDefault();
+    controller.adiciona()
+})
